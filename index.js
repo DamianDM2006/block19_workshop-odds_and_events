5212e4d7372af0a91764816094e1a7eb5ffe0df3
Damian Martinez <DCAM.DAMIAN.DM@GMAIL.COM>
2025-11-18 23:28:29 -0700

    problems:  unable to track errors

--- index.js
+++ index.js
@@ -5,53 +5,57 @@
 // - Event listeners modify state. They do not directly modify the document.
 
 // === STATE ===
-  // --- [(number bank), (odd numbers), (even number)]
+// --- [(number bank), (odd numbers), (even number)]
+const bank = [];
+console.log(`bank:`, bank);
+const oddNums = [];
+console.log(`oddNums:`, oddNums);
+const evenNums = [];
+console.log(`evenNums:`, evenNums);
 
-// === Provide Form ===
-const getNumberForm = (fromInput) => {
+// === Provide Form to input numbers ===
+const getNumberForm = () => {
   const $form = document.createElement("form");
   $form.innerHTML = `
-   <form>
-    <label>Add a Number to the Bank</label>
+    <label for="addNum">Number to Add to the Bank</label>
       <input
         type="number"
-        maxlength="10"/>
-    <button>Add Number</button>
-      <input
-        type="button"
-        id="add-num"/>
-    <button>Sort 1</button>
-      <input
-        type="button"
-        id="sort1"/>
-    <button>Sort All</button>
-      <input
-        type="button"
-        id="sort-all"/>
-  </form>
-  `
-  /* event listener */
-  /* event.preventDefault() */
-
-  return $form;
+        id="addNum"
+        name="addNum"/>
+      <button name="add">ADD</button>
+      <button name="srtOne">Sort 1</button>
+      <button name="srtAll">Sort All</button>
+  `;
+  return ($form);
 };
+const bankForm = getNumberForm();
+console.log(bankForm);
+
 
 // === FUNCTION Add Number (toBank)===
-const addNumber = (toBank) => {
-/*  <button>Add Number</button>
-      <input
-        type="button"
-        id="add-num"/>
- */};
+const addNumber = () => {
+  bankForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+  const $userNum = document.querySelector(`input`)
+
+  const toBank = bank.push($userNum)
+
+
+
+
+  });
+console.log(`TEST`, $userNum);
+ /*  return bank; */
+};
+/* addNumber(); */
 
 // === Display Inputted Numbers ===
-const displayNumbers = (fromWhere, toWhere) => {
-
-};
+const displayNumbers = (fromWhere, toWhere) => {};
 
 // === FUNCTION Sort First Number (number bank)
-const sortNumber = (fromBank) => {
-/*   <button>Sort 1</button>
+const sortNumber = () => {
+
+  /*   <button>Sort 1</button>
       <input
         type="button"
         id="sort1"/> */
@@ -64,7 +68,6 @@ const sortAll = (fromBank) => {
     <input
       type="button"
       id="sort-all"/> */
-
 };
 
 // === FUNCTION Odd or Even ===
@@ -75,5 +78,11 @@ const oddEven = (fromBank) => {
 
 // === FUNCTION  Render "when STATE changes" ===
 const render = () => {
-
+  const $app = document.querySelector("#app");
+  $app.innerHTML = `
+    <h2>Odds & Evens</h2>
+    <EnterNumber></EnterNumber>
+  `;
+  $app.querySelector("EnterNumber").replaceWith(getNumberForm());
 };
+render();
