e72bc73599e07c9cdf9915096d79d6a683619824
Damian Martinez <DCAM.DAMIAN.DM@GMAIL.COM>
2025-11-20 15:50:31 -0700

    updated index.js: now able to add inputted number to bank

--- index.js
+++ index.js
@@ -16,50 +16,51 @@ console.log(`evenNums:`, evenNums);
 // === Provide Form to input numbers ===
 const getNumberForm = () => {
   const $form = document.createElement("form");
+  $form.id = `userIn`;
   $form.innerHTML = `
     <label for="addNum">Number to Add to the Bank</label>
       <input
         type="number"
         id="addNum"
+        min="1"
         name="addNum"/>
       <button name="add">ADD</button>
       <button name="srtOne">Sort 1</button>
       <button name="srtAll">Sort All</button>
   `;
-  $form.addEventListener(`submit`, (event) => {
-    event.preventDefault(`input`);
-    const $userNum = document.querySelector(`#addNum`);
-    const $userNumber = Number($userNum);
-    const toBank = bank.push(($userNumber));
-    
-  });
-  return ($form);
+
+  return $form;
 };
 const bankForm = getNumberForm();
 console.log(bankForm);
 
-
 // === FUNCTION Add Number (toBank)===
 const addNumber = () => {
-  
-  
+  const addButton = bankForm.querySelector(`button[name="add"]`);
+  bankForm.addEventListener(`submit`, (event) => {
+    if (event.submitter == addButton) {
+      event.preventDefault();
+      const $userNum = bankForm.querySelector(`#addNum`);
+      const $userNumber = $userNum.value;
+      const toBank = bank.push(Number($userNumber));
+      bankForm.reset();
+    }
+  });
 
-/* console.log(`userNum`, userNum); */
-/*  return bank; */
+  return bankForm;
 };
-addNumber();
 
-// === Display Inputted Numbers ===
+// === Display Inputted Numbers [bank] ===
 const displayNumbers = (fromWhere, toWhere) => {};
 
 // === FUNCTION Sort First Number (number bank)
 const sortNumber = () => {
+console.log(bank);
+  const firstNum = bank.shift();
+  console.log(firstNum);
 
-  /*   <button>Sort 1</button>
-      <input
-        type="button"
-        id="sort1"/> */
 };
+sortNumber();
 
 // === FUNCTION Sort All Numbers (number bank)
 const sortAll = (fromBank) => {
@@ -83,6 +84,6 @@ const render = () => {
     <h2>Odds & Evens</h2>
     <EnterNumber></EnterNumber>
   `;
-  $app.querySelector("EnterNumber").replaceWith(getNumberForm());
+  $app.querySelector("EnterNumber").replaceWith(addNumber());
 };
 render();
